ca2b79a49aa8ae498a1fc4148e810f8dc343e839
Jon Barney <barneyj@pike.nscl.msu.edu>
2017-01-15 18:19:58 -0500

    setup_dir

--- setup_dir.sh
+++ setup_dir.sh
@@ -18,3 +18,20 @@ echo "please create ridf link to folder containing ridf files:"
 echo "ln -sf RIDF/FILE/LOCATION ridf"
 cd ../macros
 ./setup_macros_dir.sh
+cd ../
+PS3="Please choose your working environment:"
+select option in S015 RIKENHPC wait quit
+do
+    case $option in
+        S015)
+            echo "S015";;
+        Fishtank)
+            echo "Fishtank";;
+        RIKENHPC)
+            echo "RIKENHPC";;
+        MSUHPC)
+            echo "MSUHPC";;
+        other)
+            echo "Please link ridf folder manually";;
+     esac
+done
