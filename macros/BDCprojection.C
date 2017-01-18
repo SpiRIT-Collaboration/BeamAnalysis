336d12bfc053f59858241c35370437b9fcd11f6c
Jon Barney <barneyj@steelhead.nscl.msu.edu>
2017-01-18 14:02:16 -0500

    BDC work

--- macros/BDCprojection.C
+++ macros/BDCprojection.C
@@ -8,19 +8,20 @@ Double_t dist_BDCs = BDC2_z-BDC1_z; //mm
 Double_t dist_BDC1_TGT = TGT_z-BDC1_z; //mm
 Double_t pi = 3.14159;
 Double_t *MagStep(Double_t Mdz,Double_t MBrho,Double_t MB,Double_t Ma){
-  Double_t Arr[8]={-9999.,-9999.};//output:dx, a2
+  Double_t Arr[2]={-9999.,-9999.};//output:dx, a2
   if(abs(MB)>0.){
     Double_t Mrho=MBrho/MB;
     Double_t z_norm= Mdz/Mrho;
     Arr[0]=Mdz*(1.-2.*std::sin(Ma/1000.));//dx
     Arr[1]=pi*(std::sin(Ma/1000.)-1.)*z_norm/2.;//a2
   }
-  else(){
+  else{
     Arr[0]=std::tan(Ma)*Mdz;
     Arr[1]=Ma;
   }
   return Arr;
 }
+
 void BDCprojection(Int_t runNo = 3202, Int_t neve_max=30000000)
 {
 
