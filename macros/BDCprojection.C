6a35f54db3d21b61b50de135482dca37a20aa43f
Jon Barney <barneyj@walleye.nscl.msu.edu>
2017-01-24 20:57:52 -0500

    BDC

--- macros/BDCprojection.C
+++ macros/BDCprojection.C
@@ -21,7 +21,7 @@ Double_t *MagStep(Double_t Mdz,Double_t MBrho,Double_t MB,Double_t Ma){
   Double_t static Arr[2];//output:dx, da in mm, mrad
   if(abs(MB)>0.){
     Double_t Mrho=MBrho/MB*1000.;//mm
-    Ma=Ma+(std::asin(Mdz/Mrho)*1000.);//da, mrad
+    Ma=(Ma+(std::asin(Mdz/Mrho)*1000.))/2.;//da, mrad
   }
   Arr[0]=-Mdz*std::tan(Ma/1000.);//dx, mm - this is a linear approximation
   Arr[1]=Ma;
