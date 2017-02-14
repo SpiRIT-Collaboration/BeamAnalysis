cd2c2d9ce6131fccbf29a1a25bf80815ddfea41e
Jon Barney <barneyj@steelhead.nscl.msu.edu>
2017-02-14 12:49:49 -0500

    Vertex comparison macro (basic shell)

--- macros/VertexCompare.C
+++ macros/VertexCompare.C
@@ -7,34 +7,33 @@
   }
   TGTmag -> AddFile(Form("./output/BDC/BDCout.%i.ridf.root",runNo));
 
-  cbmsim -> AddFriend(TGTMag);
+  cbmsim -> AddFriend(TGTmag);
 
-  TClonesArray *STVertex = nullptr;
-  cbmsim -> SetBranchAddress("STVertex", &STVertex);
+  TClonesArray *vertexArray = nullptr;
+  cbmsim -> SetBranchAddress("STVertex", &vertexArray);
 
-  TVector3 *TPCpos;
-  STVertex->SetBranchAddress("fPos",&TPCpos);
-  
+  //TClonesArray *TPCpos;
+  //cbmsim->SetBranchAddress("STVertex.fPos",&TPCpos);
+  /*
   auto *cvs1 = new TCanvas("cvs", "", 800, 800);
   cvs1->Divide(2,2);
   cvs1->cd(1);
   cbmsim -> Draw("TGT_y_0_5T:TGT_x_0_5T >> hist(300, -100, 100, 300, -330, -130)", "", "colz");
   cvs1->cd(2);
   cbmsim -> Draw("STVertex.fPos.Y():STVertex.fPos.X() >> hist(300, -100, 100, 300, -330, -130)", "STVertex.fPos.Z()>-18&&STVertex.fPos.Z()<-5", "colz");
+  */  
   Int_t nEvents = cbmsim -> GetEntries();
   cout << "Number of events: " << nEvents << endl;
 
-  for(int iEvent=0; iEvent<nEvents; iEvents++){
-    cbmsim->GetEnetry(iEvent);
-    cout << TPCpos.X() << endl;
+  for(int iEvent=0; iEvent<10; iEvent++){
+    cbmsim->GetEntry(iEvent);
+    Int_t nVertices = vertexArray -> GetEntries();
+    if (nVertices != 1)
+      continue;
+
+    STVertex *vertex = (STVertex *) vertexArray -> At(0);
+    TVector3 posVertex = vertex -> GetPos();
+    cout << posVertex.X() << endl;
   }
 
-/*
-
-   auto c = new TChain("cbmsim");
-   for (Int_t i = 0; i < 42; i++) {
-   c -> AddFile(Form("/mnt/spirit/analysis/changj/SpiRITROOT/macros/data/run2843_s%d.reco.v1.04.root", i));
-   }
-   //c -> Draw("STVertex.fPos.Y():STVertex.fPos.X() >> hist(300, -100, 100, 300, -330, -130)", "STVertex.fPos.Z()>-18&&STVertex.fPos.Z()<-5", "colz");
-*/
  }
